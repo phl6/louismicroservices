c254bfc2b9432858bdd224257e3f14e274df3a39
Louis <louis@aishk.com>
2022-02-26 17:29:00 +0800

    [SETUP] second service: Fraud

--- customer/src/main/java/com/louiscode/customer/Customer.java
+++ customer/src/main/java/com/louiscode/customer/Customer.java
@@ -22,7 +22,8 @@ public class Customer {
     @Id
     @SequenceGenerator(
             name = "customer_id_sequence",
-            sequenceName = "customer_id_sequence"
+            sequenceName = "customer_id_sequence",
+            allocationSize = 1
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
---
+++ fraud/src/main/java/com/louiscode/fraud/Fraud.java
@@ -0,0 +1,4 @@
+package com.louiscode.fraud;
+
+public class Fraud {
+}
