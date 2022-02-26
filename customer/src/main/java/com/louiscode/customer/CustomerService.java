4ea463ef7446c9a775becd20b1e6557f1bed74c2
Louis <louis@aishk.com>
2022-02-26 20:04:06 +0800

    5.RestTemplate - RestTemplate created

--- customer/src/main/java/com/louiscode/customer/CustomerService.java
+++ customer/src/main/java/com/louiscode/customer/CustomerService.java
@@ -2,12 +2,14 @@ package com.louiscode.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService{
 
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -18,7 +20,20 @@ public class CustomerService{
         //todo: check if email valid
         //todo: check if email not taken
         //todo: store customer in db
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+        //todo: check if fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "https://localhost:8082/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if(fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("fraudster");
+        }
+        //todo: send notification
+
+
 
     }
 }
---
+++ customer/src/main/java/com/louiscode/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.louiscode.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
