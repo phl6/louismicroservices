1c217731c5c0bbd239c470d39878cce99e435162
Louis <louis@aishk.com>
2022-02-27 00:04:41 +0800

    [REFACTOR] refactor code with openfeign

--- customer/src/main/java/com/louiscode/customer/FraudCheckResponse.java
+++
@@ -1,4 +0,0 @@
-package com.louiscode.customer;
-
-public record FraudCheckResponse(Boolean isFraudster) {
-}
--- fraud/src/main/java/com/louiscode/fraud/FraudCheckResponse.java
+++
@@ -1,4 +0,0 @@
-package com.louiscode.fraud;
-
-public record FraudCheckResponse(Boolean isFraudster) {
-}
